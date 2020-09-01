fb76d2404e2ee4e0a7611dddcc675f4139de402b
jose <joserinc2@gmail.com>
2020-09-01 02:30:02 -0400

    Guardados los utlimos cambios

--- mushroomreact/src/index.js
+++ mushroomreact/src/index.js
@@ -17,23 +17,45 @@ class MyToDoList extends React.Component {
     super(props);
     // change code below this line
     this.state = {
-      userInput: '',
-      toDoList: []
+      userInput: 'Buena Suerte',
+      toDoList: [], 
+      diceOnePath: "images/dice6.png", 
+      diceTwoPath: 'images/dice6.png', 
+      messageWinner: 'Buena Suerte 😊'
     }
     // change code above this line
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
   }
   handleSubmit() {
-    const itemsArray = this.state.userInput.split(',');
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    let randomDiceImage1 = "dice" + randomNumber1 + ".png";
+    let randomImageSource1 = "images/" + randomDiceImage1;
+    let randomDiceImage2 = "dice" + randomNumber2 + ".png";
+    let randomImageSource2 = "images/" + randomDiceImage2;
     this.setState({
-      toDoList: itemsArray
+      diceOnePath: randomImageSource1, 
+      diceTwoPath: randomImageSource2
     });
-  }
-  handleChange(e) {
-    this.setState({
-      userInput: e.target.value
-    });
+
+    if (randomNumber1> randomNumber2){
+      this.setState({
+        messageWinner: "🚩 Jugador 1 Gana!"
+      });
+    }
+    else if (randomNumber1 <  randomNumber2){
+      this.setState({
+        messageWinner: "Jugador 2 Gana! 🚩"
+      });
+    }
+    else{
+      this.setState({
+        messageWinner: "Empate 😮"
+      });
+
+    }
+
+
   }
   render() {
     const items = this.state.toDoList.map(i => <li>{i}</li>); // change code here
@@ -41,18 +63,18 @@ class MyToDoList extends React.Component {
 
       <div>
         <div class="container">
-          <h1> Refresh Me </h1>
+          <h1> Jose Tira los dados!</h1>
+          <h1>{this.state.messageWinner}</h1>
           <div class="dice">
             <p> Player 1</p>
-            <img class="img1" src="images/dice1.png"></img>
+            <img class="img1" src={this.state.diceOnePath}></img>
           </div>
           <div class="dice">
             <p> Player 2</p>
-            <img class="img2" src="images/dice4.png"></img>
+            <img class="img2" src={this.state.diceTwoPath}></img>
           </div>
         </div>
-        <h1>My "To Do" List:</h1>
-        <button onClick={this.handleSubmit}>Roll the Dice!</button>
+        <button class="button" onClick={this.handleSubmit}>Roll the Dice!</button>
         <ul>
           {items}
         </ul>
